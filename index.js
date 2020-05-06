566815e015bd10a5030c5e688642b2d7ba50c48d
Rohit Kale <ruk22@cam.ac.uk>
2020-05-06 00:11:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 function breakOut(array, changeValue, stopValue) {
   for (let i = 0; i < array.length; i++) {
-    if (array[i] === "stopValue") {
+    if (array[i] === stopValue) {
       break
     }
-    array[i] = "changeValue";
+    array[i] = changeValue;
   }
 }
 
